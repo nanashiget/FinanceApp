95deda30c63051c545fd18407219545ed293b310
nanashiget <155833397+nanashiget@users.noreply.github.com>
2026-09-17 17:40:52 +0500

    fix: use ColumnScope weight extension

--- app/src/main/java/com/example/financeapp/presentation/common/components/TransactionsSectionScreen.kt
+++ app/src/main/java/com/example/financeapp/presentation/common/components/TransactionsSectionScreen.kt
@@ -4,14 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
