04660351c5832de2fdba9c2d96478bcd3a9d32cb
nanashiget <155833397+nanashiget@users.noreply.github.com>
2026-09-17 17:35:52 +0500

    fix: import planner JSON decoder

--- app/src/main/java/com/example/financeapp/data/repository/PlannerPreferencesRepository.kt
+++ app/src/main/java/com/example/financeapp/data/repository/PlannerPreferencesRepository.kt
@@ -14,6 +14,7 @@ import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
